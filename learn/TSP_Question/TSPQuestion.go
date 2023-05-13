15fa90f571886a2a991bdf4890979c276b10263e
Cpouple <1608737736@qq.com>
2023-05-13 11:52:23 +0800

    TSP_Question

--- learn/TSP_Question/TSPQuestion.go
+++ learn/TSP_Question/TSPQuestion.go
@@ -6,9 +6,9 @@ import (
 	"math/rand"
 )
 
-type branch struct {
-	Length int    // 表示该分支所代表的路径总长度
-	Path   []int  // 表示该分支所代表的经过的城市序列
+type embranchment struct {
+	length int    // 表示该分支所代表的路径总长度
+	path   []int  // 表示该分支所代表的经过的城市序列
 	visit  []bool // 记录每个城市是否被访问过的布尔数组
 }
 
@@ -25,7 +25,7 @@ func Test() (err error) {
 	startCity := rand.Intn(cityNum)
 	path, length := TspBfs(cityDist, startCity)
 	fmt.Println("旅行家走的路径: ")
-	printPath(path, length)
+	PrintPath(path, length)
 	return
 }
 
@@ -60,85 +60,76 @@ func CityDistance(cityMap [][]int) {
 	}
 }
 
-func printPath(path []int, length int) {
+func PrintPath(path []int, length int) {
 	pathLen := len(path)
 	for i := 0; i < pathLen; i++ {
 		fmt.Print(path[i] + 1)
 		if i != pathLen-1 {
-			fmt.Print("-")
+			fmt.Print("->")
 		}
 	}
 	fmt.Println()
 	fmt.Println(length)
 }
 
-func TspBfs(cityMap [][]int, startCity int) (path []int, length int) {
-	cityNum := len(cityMap)
+func TspBfs(cityDist [][]int, beginCity int) (path []int, length int) {
+	cityNum := len(cityDist)
 	// 创建一个优先队列用于存储搜索树中的分支
-	queue := garray.NewSortedArray(func(a, b any) int {
-		aLength := a.(*branch)
-		bLength := b.(*branch)
+	array := garray.NewSortedArray(func(a, b any) int {
+		aLength := a.(*embranchment)
+		bLength := b.(*embranchment)
 		// 根据总长度比较两个分支
-		if aLength.Length < bLength.Length {
+		if aLength.length < bLength.length {
 			return -1
 		}
 		// 如果两个分支长度相等，则根据路径长度比较它们
-		if aLength.Length == bLength.Length {
-			if len(aLength.Path) > len(bLength.Path) {
+		if aLength.length == bLength.length {
+			if len(aLength.path) > len(bLength.path) {
 				return -1
 			}
-			if len(aLength.Path) == len(bLength.Path) {
+			if len(aLength.path) == len(bLength.path) {
 				return 0
 			}
 		}
 		return 1
 	})
 	// 创建一个初始的分支，包含起始城市，并将其添加到优先队列中
-	startB := &branch{
-		Length: 0,
-		Path:   []int{startCity},
+	startB := &embranchment{
+		length: 0,
+		path:   []int{beginCity},
 		visit:  make([]bool, cityNum, cityNum),
 	}
-	startB.visit[startCity] = true
-	queue.Add(startB)
-	currLength := 0
-	currPathMinLength := 0
+	startB.visit[beginCity] = true
+	array.Add(startB)
 	// 循环直到找到TSP最短路径或搜索所有可能的路径
-	for queue.Len() > 0 {
-		bTemp, found := queue.PopLeft()
+	for array.Len() > 0 {
+		bTemp, found := array.PopLeft()
 		if !found {
 			return
 		}
-		b := bTemp.(*branch)
-		// 根据弹出的分支更新当前最长的路径和当前最短路径长度
-		if b.Length > currLength {
-			currLength = b.Length
-			currPathMinLength = len(b.Path)
-		} else if len(b.Path) < currPathMinLength {
+		b := bTemp.(*embranchment)
+		if len(b.path) == cityNum {
+			b.length += cityDist[b.path[len(b.path)-1]][beginCity]
+			b.path = append(b.path, beginCity)
+			array.Add(b)
 			continue
 		}
-		switch len(b.Path) {
-		case cityNum:
-			b.Length += cityMap[b.Path[len(b.Path)-1]][startCity]
-			b.Path = append(b.Path, startCity)
-			queue.Add(b)
-			continue
-		case cityNum + 1:
-			path = b.Path
-			length = b.Length
+		if len(b.path) == cityNum+1 {
+			path = b.path
+			length = b.length
 			return
 		}
 		for i := 0; i < cityNum; i++ {
-			if b.visit[i] || cityMap[b.Path[len(b.Path)-1]][i] == 0 {
+			if b.visit[i] || cityDist[b.path[len(b.path)-1]][i] == 0 {
 				continue
 			}
-			nextPath := append([]int{}, b.Path...)
+			nextPath := append([]int{}, b.path...)
 			nextVisitPlace := append([]bool{}, b.visit...)
 			nextPath = append(nextPath, i)
 			nextVisitPlace[i] = true
-			queue.Add(&branch{
-				Length: b.Length + cityMap[b.Path[len(b.Path)-1]][i],
-				Path:   nextPath,
+			array.Add(&embranchment{
+				length: b.length + cityDist[b.path[len(b.path)-1]][i],
+				path:   nextPath,
 				visit:  nextVisitPlace,
 			})
 		}
